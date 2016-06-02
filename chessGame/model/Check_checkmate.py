cbc6a3257b297e7649e1d95b0c1a70a58560552a
jungsugi <sooyoung199@naver.com>
2016-06-02 18:55:01 +0900

    -ing

---
+++ chessGame/model/Check_checkmate.py
@@ -0,0 +1,3 @@
+Python 3.5.1 |Anaconda 4.0.0 (64-bit)| (default, Feb 16 2016, 09:49:46) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> sdasd
